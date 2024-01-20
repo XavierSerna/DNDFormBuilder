a0f1aad2fbc9f6bf4ea25ddf30ccbf765ad9165a
xavier <xavierserna472@gmail.com>
2024-01-20 12:01:57 -0500

    second commit (unstable)

--- app/Livewire/FormBuilder.php
+++ app/Livewire/FormBuilder.php
@@ -3,11 +3,123 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+//use Livewire\Attributes\On;
+use Illuminate\Support\Facades\Log;
+
+$miVariable =  "valor";
+Log::info('Contenido de miVariable:', $miVariable);
 
 class FormBuilder extends Component
 {
+
+    public $droppedItems = []; 
+
+    #[On('addField')] 
+    public function addField($type)
+    {
+        $this->droppedItems[] = $type;
+        Log::info('addField recibió:', ['type' => $type]);
+    }
+
+/*     #[On('addField')]
+    public function addField($type)
+    {
+        \Log::info("Received type in addField: {$type}");
+    } */
+
+/*
+    protected $listeners = ['addField'];
+
+    public function addField($type)
+    {
+    \Log::debug("addField called with type: {$type}");
+
+    // Add a simple hardcoded array to the droppedItems list for testing
+    $this->droppedItems[] = [
+        'type' => 'text', // Hardcoded type
+        'placeholder' => 'Hardcoded placeholder', // Hardcoded placeholder
+        'name' => 'hardcoded_name_' . time(), // Hardcoded name with a unique suffix
+    ];
+    }
+*/
+
+    /*
+    //#[On('addField')] 
+    public function addField($type)
+    {
+    // Initialize default values for placeholder and name
+    $placeholder = '';
+    $name = '';
+
+    // Determine the placeholder and name based on the type of the field
+    switch ($type) {
+        case 'text':
+            $placeholder = 'Enter text';
+            $name = 'text_field_' . time(); // Using time() to ensure a unique name
+            break;
+
+        case 'textarea':
+            $placeholder = 'Enter additional information';
+            $name = 'textarea_field_' . time();
+            break;
+
+        case 'checkbox':
+            // For checkboxes, you may not need a placeholder
+            $name = 'checkbox_field_' . time();
+            break;
+
+        case 'email':
+            $placeholder = 'Enter email address';
+            $name = 'email_field_' . time();
+            break;
+
+        case 'number':
+            $placeholder = 'Enter number';
+            $name = 'number_field_' . time();
+            break;
+    }
+        // Add the new field to the droppedItems array
+        $this->droppedItems[] = [
+            'type' => $type,
+            'placeholder' => $placeholder,
+            'name' => $name,
+        ];
+    }
+    */
+
+    /*
+    public function addField($type)
+    {
+        // Logic to add a new field of type $type
+        // This could involve updating an array or a database record
+
+        // Add a new item to the array with the type set
+        $this->droppedItems[] = [
+            'type' => $type,
+            'placeholder' => '', // perhaps set this dynamically or based on the type
+            'name' => '', // Same here for the name
+        ];
+    }
+    */
+    
+    public $things = [
+        ['id' => 1, 'type' => 'email', 'label' => 'Email'],
+        ['id' => 2, 'type' => 'radio', 'label' => 'Radio'],
+        ['id' => 3, 'type' => 'checkbox', 'label' => 'Checkbox'],
+        ['id' => 4, 'type' => 'dropdown', 'label' => 'Dropdown'],
+        ['id' => 5, 'type' => 'textarea', 'label' => 'Textarea'],
+    ];
+    
+    public function reorder($orderedIds)
+    {
+        $this->things = collect($orderedIds)->map(function ($id) {
+            return collect($this->things)->where('id', (int) $id['value'])->first();
+        })->toArray();
+    }
+
     public function render()
     {
         return view('livewire.form-builder');
     }
+
 }
--- resources/views/livewire/form-builder.blade.php
+++ resources/views/livewire/form-builder.blade.php
@@ -1,31 +1,84 @@
 <div>
-<h4>Knowing others is intelligence; knowing yourself is true wisdom.</h4>
 
-<!-- Field Types Area: This is where the draggable field types will be displayed -->
+<!-- Field Types Area: the draggable field types will be displayed -->
 <div class="d-flex flex-wrap justify-content-start mb-4">
-    <!-- Loop through each field type and create a draggable element for it -->
+    <!-- Loop through each type and create a element. (associative array is embedded within foreach) -->
     @foreach(['text' => 'Text', 'textarea' => 'Textarea', 'checkbox' => 'Checkbox', 'radio' => 'Radio', 'select' => 'Dropdown', 'number' => 'Number', 'email' => 'Email', 'tel' => 'Phone'] as $type => $label)
-        <!-- Draggable Field Button: Each button is styled with Bootstrap and has a 'draggable' attribute -->
+        <!-- Draggable Field Button ('draggable' attribute) -->
         <div class="p-2">
-            <button class="btn btn-outline-primary draggable-field" draggable="true" data-type="{{ $type }}">
+            <!-- it gives a [$type] value to hold to each button-->
+            <button class="btn btn-outline-primary draggable-field" draggable="true" data-type="{{ $type }}" 
+            ondragstart="startDrag(event, '{{ $type }}')">
+                <!-- sets the text of each button based on the [$label] variable -->
                 {{ $label }}
             </button>
         </div>
     @endforeach
 </div>
 
-<!-- Form Construction Area: This is the area where the user will drag fields to construct the form -->
-<div class="form-construction-area border p-3 mb-4" id="formConstructionArea">
-    <!-- Placeholder Text: Informs users to drag fields here -->
-    <p>Drag and drop the fields here to build your form.</p>
-    <!-- Dynamic Form Fields: This area will be updated dynamically as fields are dragged into it -->
-    <!-- You will handle this in your Livewire component's PHP class -->
+<div class="droppable-area p-3 mb-4 border rounded bg-light"
+    ondrop="drop(event)"
+    ondragover="allowDrop(event)">
+    <!-- 
+    @foreach($droppedItems as $droppedItem)
+        @switch($droppedItem['type'])
+        @case('text')
+            <input type="text" placeholder="{{ $droppedItem['placeholder'] ?? '' }}">
+            @break
+        @case('textarea')
+            <textarea placeholder="{{ $droppedItem['placeholder'] ?? '' }}"></textarea>
+            @break
+        @case('checkbox')
+            <input type="checkbox" name="{{ $droppedItem['name'] ?? '' }}">
+            @break
+        @default
+        @endswitch
+    @endforeach
+     -->
+    @foreach ($droppedItems as $droppedItem)
+    <div>
+        tipo de campo: {{ $droppedItem }}
+    </div>
+    @endforeach
 </div>
-    
-<!-- Action Buttons: These buttons perform actions like saving the form -->
-<div class="d-flex justify-content-end">
-    <!-- Save Form Button: Triggers a method in the Livewire component to save the form -->
-    <button class="btn btn-success" wire:click="saveForm">Save Form</button>
+
+
+<div class="container p-3 mb-4">
+    <ul wire:sortable="reorder" class="list-group">
+        @foreach ($things as $thing)
+            <li wire:sortable.item="{{ $thing['id'] }}" draggable="true" wire:key="{{ $thing['id'] }}" class="list-group-item">
+                @switch($thing['type'])
+                    @case('email')
+                        <input type="email" placeholder="{{ $thing['label'] }}">
+                    @break
+                    @case('radio')
+                        <div>
+                            <input type="radio" name="radio_option" value="option1"> Hungry<br>
+                            <input type="radio" name="radio_option" value="option2"> Not Hungry<br>
+                        </div>
+                    @break
+                    @case('checkbox')
+                        <div>
+                            <input type="checkbox" name="checkbox_option1" value="option1">meat<br>
+                            <input type="checkbox" name="checkbox_option2" value="option2">vegetables<br>
+                            <input type="checkbox" name="checkbox_option3" value="option3">water<br>
+                        </div>
+                    @break
+                    @case('dropdown')
+                        <select>
+                            <option value="option1">quinua</option>
+                            <option value="option2">potato</option>
+                            <option value="option3">chicken</option>
+                        </select>
+                    @break
+                    @case('textarea')
+                        <textarea placeholder="Additional Information for your dish"></textarea>
+                    @break
+
+                @endswitch
+            </li>
+        @endforeach
+    </ul>
 </div>
 
 </div>
